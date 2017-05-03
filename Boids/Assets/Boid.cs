8d66df120c16b87078234d7c6acb6ea66d98c2b7
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-05-03 22:35:04 +0800

    Chapter 26: An OOP Boid implementation, Part 2. Still have problems of Boids running to each other

--- Boids/Assets/Boid.cs
+++ Boids/Assets/Boid.cs
@@ -18,6 +18,8 @@ public class Boid : MonoBehaviour
     public Boid ClosestBoid; // The closest Boid
 
     // Initialize this Boid on Awake
+    // 1: The Awake() function is called by Unity at the moment that this GameObject is instantiated.
+    // This means that it is called beofre Start()
     void Awake()
     {
         // Define the Boids list if it is still null
@@ -25,6 +27,9 @@ public class Boid : MonoBehaviour
         {
             Boids = new List<Boid>();
         }
+        // ^2: All instances of the Boid class can access the shared static List<Boid> Boid.
+        // The first Boid to be created will initialize a new List<Boid>, and then the others
+        // will just add themselves to the list
 
         // Add this Boid to Boids
         Boids.Add(this);
@@ -40,10 +45,16 @@ public class Boid : MonoBehaviour
         // Initialize the two lines
         Neighbors = new List<Boid>();
         CollisionRisk = new List<Boid>();
+        // ^3: Each Boid maintains its own list of neighbors and collision risks.
+        // The neighbors are other Boids that are within BoidSpawner.S.nearDist of this Boid
+        // The collision risks are those that are within BoidSpawner.S.collisionDist
 
         // Make this.tranform a child of the Boids GameObjetc
         this.transform.parent = GameObject.Find("Boids").transform;
-        //^4
+        //^4: Making all the Boids children of the same GameObject will help keep the Hierarchy pane organized
+        // It places them all underneath a single parent GameObject named Bolds.
+        // If you want to see them all listed in the hierarchy, you just need to click the disclosure triangle
+        // next to the parent object Boids.
 
         // Give the Boid a random color, but make sure it's not too dark
         Color randColor = Color.black;
@@ -65,26 +76,91 @@ public class Boid : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-    // 5
+    // 5: Every Update(), each Boid needs to find its neighbors and react to their locations using
+    // collision avoidance, velocity matching and flock centering.
 	void Update () {
 		
         // Get the list of nearby Boids (this Boid's neighbors)
-	    List<Boid> neighbors = GetNeighbors(this);
-        // ^6
+	    List<Boid> Neighbors = GetNeighbors(this);
+        // ^6: GetNeighbors function is defined later in the code, it finds other Boids that are nearby
 
         // Initialize newVelocity and newPosition to the current values
 	    NewVelocity = Velocity;
 	    NewPosition = this.transform.position;
 
         // Velocity matching: this sets the velocity of the Boid to be similar to that of its neighbors
+	    Vector3 neighborVel = GetAverageVelocity(Neighbors);
+        // Utilizes the fields set on the Boidspawner.S singleton
+	    NewVelocity = neighborVel * BoidSpawner.S.VelocityMatchingAmt;
 
+        // Flock Centering: Move toward middle of neighbors
+	    Vector3 neighborCenterOffset = GetAveragePosition(Neighbors) - this.transform.position;
+	    NewVelocity += neighborCenterOffset * BoidSpawner.S.FlockCenteringAmt;
 
+        // Collision avoidance: Avoid running into Boids that are too close
+	    Vector3 dist;
+	    if (CollisionRisk.Count > 0)
+	    {
+	        dist = GetAveragePosition(CollisionRisk) - this.transform.position;
+	        NewVelocity += dist * BoidSpawner.S.CollisionAvoidanceAmt;
+	    }
 
+        // Mouse attraction, move toward the mouse position no matter how far away
+	    dist = BoidSpawner.S.MousePos - this.transform.position;
+	    if (dist.magnitude > BoidSpawner.S.MouseAvoidanceDist)
+	    {
+	        NewVelocity += dist * BoidSpawner.S.MouseAttractionAmt;
+	    }
+
+        // Avoid the mouse if it is too close
+	    else
+	    {
+	        NewVelocity += dist.normalized * BoidSpawner.S.MouseAvoidanceDist * BoidSpawner.S.MouseAvoidanceAmt;
+	    }
+
+        // newVelocity and newPosition are ready, but wait unitl LateUpdate() 
+        // to set them so that this Boid does not move before others have 
+        // had a chance to calculate their new value
 	}
+
+    // By allowing all Boids to Update() themselves before any Boids move, we avoid
+    // race conditions that could be caused by some Boids moving before others have decided where to go
+
+    // 7: LateUpdate is called after Update has been called on every object
+
+    void LateUpdate()
+    {
+        // Adjust the current velocity based on newVelocity using a linear interpolation ( see Appendix B, "Useful Concept")
+        Velocity = (1 - BoidSpawner.S.VelocityLerpAmt) * Velocity + BoidSpawner.S.VelocityLerpAmt * NewVelocity;
+
+        // Make sure the velocity is in the max and min range of it
+        if (Velocity.magnitude > BoidSpawner.S.MaxVelocity)
+        {
+            Velocity = Velocity.normalized * BoidSpawner.S.MaxVelocity;
+        }
+        if (Velocity.magnitude < BoidSpawner.S.MinVelocity)
+        {
+            Velocity = Velocity.normalized * BoidSpawner.S.MinVelocity;
+        }
+
+        // Decide on the new Position
+        NewPosition = this.transform.position + Time.deltaTime * Velocity;
+
+        // Keep everything in the xz plane
+        NewPosition.y = 0;
+
+        // Look from the old position at the newPosition to orient the model
+        this.transform.LookAt(NewPosition);
+
+        // Actually move to the newPosition
+        this.transform.position = NewPosition;
+    }
 
     // Find which  Boids are near enough to be considered neighbors
     // boi is Boid of interest, the Boid on which we are focusing
-    // 8
+
+    // 8: GetNeighbors will find the nearby Boids around the boi
+
     public List<Boid> GetNeighbors(Boid boi)
     {
         float closestDist = float.MaxValue; // set the max float value
@@ -118,7 +194,9 @@ public class Boid : MonoBehaviour
         return (Neighbors);
     }
 
-    //Get the average position of the Boids in a List<Boid>
+    // Get the average position of the Boids in a List<Boid>
+    // 9: GetAveragePosition will return the average position of a group of Boids
+
     public Vector3 GetAveragePosition(List<Boid> someBoids)
     {
         Vector3 sum = Vector3.zero;
@@ -132,6 +210,7 @@ public class Boid : MonoBehaviour
     }
 
     // Get the Average velocity of the Boids in a List<Boid>
+    // 10: GetAverageVelocity will return the average velocity Vector3 of a group of Boids
     public Vector3 GetAverageVelocity(List<Boid> someBoids)
     {
         Vector3 sum = Vector3.zero;
