cca8b0c5bfdb6b6ad43710858027ecaeba8e6fb7
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-06-01 22:15:42 +0800

    add the bound union function in the utility scripts

--- [30] Space Shump/Assets/__Scripts/Utils.cs
+++ [30] Space Shump/Assets/__Scripts/Utils.cs
@@ -10,6 +10,17 @@ public class Utils : MonoBehaviour
     public static Bounds BoundsUnion(Bounds b0, Bounds b1)
     {
         // If the size of one of the bounds is vector3.zero , ignore that one
+        if (b0.size == Vector3.zero && b1.size != Vector3.zero)
+            return (b1);
+        else if (b0.size != Vector3.zero && b1.size == Vector3.zero)
+            return (b0);
+        else if (b0.size == Vector3.zero && b1.size == Vector3.zero)
+            return (b0);
 
+        // Stretch b0 to include the b1.min and b1.max
+        b0.Encapsulate(b1.min);
+        b0.Encapsulate(b1.max);
+        return (b0);
     }
+
 }
