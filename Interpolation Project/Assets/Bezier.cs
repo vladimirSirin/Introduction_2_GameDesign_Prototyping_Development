f317f36417b53dbfc573016b108a08d20ec9440c
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-07-02 00:05:27 +0800

    Finish the interpolation study

---
+++ Interpolation Project/Assets/Bezier.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bezier : MonoBehaviour
+{
+
+    public Transform c0, c1, c2, c3;
+    public float timeDuration = 1;
+
+    // Set CheckCalculate to true to start moving
+    public bool checkCalculate = false;
+
+    public bool ____________________________;
+    public float u;
+    public Vector3 p0123;
+    public bool moving = false;
+    public float timeStart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (checkCalculate)
+	    {
+	        checkCalculate = false;
+            moving = true;
+	        timeStart = Time.time;
+	    }
+
+	    if (moving)
+	    {
+	        u = (Time.time - timeStart) / timeDuration;
+	        if (u>=1)
+	        {
+	            u = 1;
+	            moving = false;
+	        }
+
+            // 4 point Bezier curve calculation
+	        Vector3 p01, p12, p23, p012, p123;
+	        p01 = (1 - u) * c0.position + u * c1.position;
+	        p12 = (1 - u) * c1.position + u * c2.position;
+	        p23 = (1 - u) * c2.position + u * c3.position;
+
+	        p012 = (1 - u) * p01 + u * p12;
+	        p123 = (1 - u) * p12 + u * p23;
+
+	        p0123 = (1 - u) * p012 + u * p123;
+
+	        transform.position = p0123;
+	    }
+		
+	}
+}
---
+++ Interpolation Project/Assets/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+
+    // The standard Vector Lerp functions in Unity don't allow for extrapolation
+    // (i.e., u is clamped to 0<=u<=1, so we write our own Lerp function
+
+    static public Vector3 Lerp(Vector3 vFrom, Vector3 vTo, float u)
+    {
+        Vector3 res = (1 - u) * vFrom + u * vTo;
+        return res;
+    }
+
+
+    // While most Bezier curves are 3 or 4 points, it is possible to have any number of points using this recursive function
+    // This use the Lerp function above because the Vector3.Lerp function
+    // doesn't allow extrapolation
+    static public Vector3 Bezier(float u, List<Vector3> vList)
+    {
+        // If there is only one element in vList, return it
+        if (vList.Count == 1)
+        {
+            return vList[0];
+        }
+
+
+        // Create vListR, which is all but the 0th element of vList
+        // e.g., if vList = [0,1,2,3,4], vListR = [1,2,3,4]
+        List<Vector3> vListR = vList.GetRange(1, vList.Count - 1);
+
+
+        // And create vListL, which is all but the last element  of vList
+        //e.g., if vList = [0,1,2,3,4], then vListL = [0,1,2,3]
+        List<Vector3> vListL = vList.GetRange(0, vList.Count - 1);
+
+        // The result is the Lerp of the Bezier of these two shorter Lists
+        Vector3 res = Lerp(Bezier(u, vListL), Bezier(u, vListR), u);
+
+        // ^ The Bezier function recursively calls itself here to split the
+        // lists down until there is only one value in each
+        return res;
+
+    }
+
+    // This version allow an array of a series of Vector3s input which is then converted into a list<Vector3>
+    static public Vector3 Bezier(float u, params Vector3[] vecs)
+    {
+        return Bezier(u, new List<Vector3>(vecs));
+    }
+
+}
