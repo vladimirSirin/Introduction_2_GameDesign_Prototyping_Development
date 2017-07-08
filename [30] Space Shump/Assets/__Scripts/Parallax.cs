ea83c76e213c7a5298db333bcf8554274a11a06a
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-07-08 22:58:30 +0800

    Added moving background

---
+++ [30] Space Shump/Assets/__Scripts/Parallax.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public GameObject poi; // the player ship
+    public GameObject[] panels; // the scrolling foreground
+    public float scrollSpeed = -30f;
+    // motionMult controls how much panels react to player movements
+    public float motionMult = 0.25f;
+
+    private float panelHt; // Height of each panel
+    private float depth; // Depth of panels ( that is, poi.z)
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    panelHt = panels[0].transform.localScale.y;
+	    depth = panels[0].transform.position.z;
+
+	    // set initial position of panels
+        panels[0].transform.position = new Vector3(0, 0, depth);
+        panels[1].transform.position = new Vector3(0, panelHt, depth);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float tY, tX = 0;
+
+	    tY = Time.time * scrollSpeed % panelHt + (panelHt * 0.5f);
+
+	    if (poi != null)
+	    {
+	        tX = -poi.transform.position.x * motionMult;
+	    }
+	    // Position panels[0]
+        panels[0].transform.position = new Vector3(tX, tY, depth);
+
+	    // Then position panels[1] where needed to make a continuous starfield
+	    if (tY >= 0)
+	    {
+	        panels[1].transform.position = new Vector3(tX, tY - panelHt, depth);
+	    }
+	    else
+	    {
+	        panels[1].transform.position = new Vector3(tX, tY + panelHt, depth);
+	    }
+	}
+}
