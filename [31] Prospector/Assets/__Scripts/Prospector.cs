981b8e21aa127a33c3a0d68abe6f4c9fb55b0403
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-07-28 23:23:25 +0800

    Positioning the cards from the deck to the tableau, assign them correspondingly to the slotdef one by one

--- [31] Prospector/Assets/__Scripts/Prospector.cs
+++ [31] Prospector/Assets/__Scripts/Prospector.cs
@@ -11,6 +11,14 @@ public class Prospector : MonoBehaviour
     public Deck deck;
 
     public TextAsset deckXML;
+    public Vector3 layoutCentre;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Transform layoutAnchor;
+
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
 
     public Layout layout;
     public TextAsset layoutXML;
@@ -37,6 +45,7 @@ public class Prospector : MonoBehaviour
         layout.ReadLayout(layoutXML.text); // Pass layout.xml to it
 
 	    drawPile = cardListToCardProspectorsList(deck.cards);
+	    LayoutGame();
 	}
 	
 	// Update is called once per frame
@@ -59,5 +68,52 @@ public class Prospector : MonoBehaviour
         }
 
         return cardProspectorList;
+    }
+
+    // The Draw function will pull a single card from the drawpile and return it 
+    CardProspector Draw()
+    {
+        CardProspector cd = drawPile[0]; // Pull the 0th CardProspector
+        drawPile.RemoveAt(0); // Then remove it from List<> drawPile
+        return cd;  // And return it
+    }
+
+    // LayoutGame() Position the initial tableau of cards, aka the "mine"
+    void LayoutGame()
+    {
+        if (layoutAnchor == null)
+
+        {
+            GameObject tempGO = new GameObject();
+            layoutAnchor = tempGO.transform;
+            layoutAnchor.position = layoutCentre;
+        }
+
+        CardProspector cp;
+
+        foreach (SlotDef slotDef in layout.slotDefs)
+        {
+            // ^Iterate through all the slotDefs in the layout.slotDefs as slotDef
+            cp = Draw(); // Pull a card from the top of the drawPile
+
+            // Set the state, hiddenBy, layoutId, slotDef, faceUp, position
+            cp.faceUp = slotDef.faceUp; // Set its faceUp to the value in SlotDef
+            cp.transform.parent = layoutAnchor; // Make its parent layoutAnchor
+            // This replaces the previous parent: deck.deckAnchor, which appears
+            // as _Deck in the Hierarchy when the scene is playing
+
+            cp.transform.localPosition = new Vector3(
+                layout.multiplier.x * slotDef.x,
+                layout.multiplier.y * slotDef.y,
+                -slotDef.layerId);
+            // ^Set the localPosition of the card based on slotDef
+
+            cp.state = CardState.tableau; // CardProspectors in the tableau have the state CardState.tableau
+            cp.layoutId = slotDef.layerId;
+            cp.slotDef = slotDef;
+            
+            tableau.Add(cp); // Add this CarProspector to the List<> tableau
+
+        }
     }
 }
