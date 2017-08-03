5f9f1f31e1cfc161a437de60c8f0f141cb373d3d
vladimirSirin <shadowsong_sirin@hotmail.com>
2017-08-03 22:31:46 +0800

    Understanding the scoreing system

---
+++ [31] Prospector/Assets/__Scripts/FloatingScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// An enum to track the possible states of a FloatingScore
+
+
+// FloatingScore can move itself on screen following a Bezier Curve
+public class FloatingScore : MonoBehaviour {
+
+
+    // The score property also sets scoreString when set
+
+
+    // The GameObject that will receive the SendMessage when this is done moving
+
+
+    // Setup the FloartingScore and movement
+    // Note the use of parameter defaults for eTimeS & eTimeD
+
+
+}
